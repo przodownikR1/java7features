cd2bd41cf88fb1ae7c3c32e2699d415a7c135dc8
Slawomir Borowiec <przodownik@tlen.pl>
2014-05-30 14:24:50 +0200

    file nio2 test example

--- src/test/java/pl/java/scalatech/test/file/FileReadWriteTest.java
+++ src/test/java/pl/java/scalatech/test/file/FileReadWriteTest.java
@@ -1,10 +1,19 @@
 package pl.java.scalatech.test.file;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileStore;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.fest.assertions.Assertions;
 import org.junit.Test;
@@ -16,6 +25,7 @@ import com.google.common.collect.Lists;
  * Module name :    java7features
  * Creating time :  30 maj 2014
  */
+@Slf4j
 public class FileReadWriteTest {
 
     @Test
@@ -27,5 +37,46 @@ public class FileReadWriteTest {
         //then
         lines = Files.readAllLines(Paths.get("bar.txt"), Charset.forName("UTF-8"));
         Assertions.assertThat(lines).contains("first","second","third");
+    }
+    
+    @Test
+    public void shouldShowFilesFromDirectory() throws IOException{
+        Files.walkFileTree(Paths.get("bin"), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                    throws IOException{
+                        log.info(" {}", file);
+                        return FileVisitResult.CONTINUE;
+                
+                
+            }
+          });
+    }
+    
+    @Test
+    public void shouldScanFolderWork(){
+       scan("bin","g");
+    }
+    
+    private static void scan(String folder, String pattern) {
+        Path dir = Paths.get(folder);
+
+        if (!Files.exists(dir) || !Files.isDirectory(dir)) {
+            log.info("No such directory!");
+        }
+
+        try (DirectoryStream<Path> ds =  Files.newDirectoryStream(dir, pattern)) {
+
+            int count = 0;
+            for (Path path : ds) {
+                log.info("path {}",path);
+                count++;
+            }
+
+            log.info(" Files match the pattern  {}"
+                                                     , count);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
